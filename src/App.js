321220daabb133134145e8949c853d176a795430
Adthasid <AdthasidSuksomthawisap@Adthasids-MacBook-Pro.local>
2017-04-05 16:08:59 +0700

    3. Add event add to-do-list

--- src/App.js
+++ src/App.js
@@ -4,11 +4,28 @@ import './App.css'
 class App extends React.Component {
 
   state = {
-    inputText: ''
+    inputText: '',
+    listItem: []
   }
 
   handleChangeText = (event) => {
     this.setState({inputText: event.target.value});
+  }
+
+  addList = () => {
+    this.setState({
+      listItem: this
+        .state
+        .listItem
+        .concat([this.state.inputText]),
+      inputText: ''
+    })
+  }
+
+  handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      this.addList();
+    }
   }
 
   render() {
@@ -22,10 +39,11 @@ class App extends React.Component {
                 type="text"
                 placeholder="I'm here!"
                 value={this.state.inputText}
-                onChange={this.handleChangeText}/>
+                onChange={this.handleChangeText}
+                onKeyPress={this.handleKeyPress}/>
             </p>
             <p className="control">
-              <button className="button">
+              <button className="button" onClick={this.addList}>
                 Add
               </button>
             </p>
@@ -35,6 +53,19 @@ class App extends React.Component {
         {this.state.inputText != '' && <div className="notification is-info">
           <h2>{this.state.inputText}</h2>
         </div>}
+
+        <aside className="menu">
+          <ul className="menu-list">
+            {this.state.listItem.map(( value, index ) => {
+              return (
+                <li className="space-1" key={ index + value }>{ value }<span className="space-1">
+                  <button className="delete"></button></span>
+                </li>
+              )
+            })}
+            
+          </ul>
+        </aside>
 
       </div>
     );
