870ad635d71fd77cead0d58f234f4ccd900726c7
Adthasid <AdthasidSuksomthawisap@Adthasids-MacBook-Pro.local>
2017-04-05 16:07:20 +0700

    2. Add event onChange, handleChangeText function

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import './App.css'
 
 class App extends React.Component {
 
+  state = {
+    inputText: ''
+  }
+
+  handleChangeText = (event) => {
+    this.setState({inputText: event.target.value});
+  }
+
   render() {
     return (
       <div className="App">
         <div className="container notification">
-          <div className="field has-addons">
+          <div className="field has-addons has-addons-centered">
             <p className="control">
-              <input className="input" type="text" placeholder="I'm here!"/>
+              <input
+                className="input"
+                type="text"
+                placeholder="I'm here!"
+                value={this.state.inputText}
+                onChange={this.handleChangeText}/>
             </p>
             <p className="control">
               <button className="button">
@@ -17,8 +30,12 @@ class App extends React.Component {
               </button>
             </p>
           </div>
-          <h2></h2>
         </div>
+
+        {this.state.inputText != '' && <div className="notification is-info">
+          <h2>{this.state.inputText}</h2>
+        </div>}
+
       </div>
     );
   }
